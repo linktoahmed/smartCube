e87717e8370624fdd300a3907db490d88918bf33
linktoahmed <linktoahmed@gmail.com>
2025-07-09 03:15:40 +0300

    small fix

--- playwright.config.js
+++ playwright.config.js
@@ -28,7 +28,7 @@ module.exports = defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://juice-shop.herokuapp.com/#/',
-    headless: false,
+    headless: true,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
